5c1050ca456f1e340781df2f76b3fedaca657d7b
Yaniv Trabelsi <yaniv@praedicta.com>
2018-12-26 17:50:05 +0200

    dfg

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 
 import * as Mocha from 'mocha'
-import {join, dirname} from 'path'
+import {join} from 'path'
 
 function runTest(url: string, tests: { name: string, tests: { type: string, query: string, expect: any }[] }[]) {
     global['specs'] = { url, tests };
     var mocha = new Mocha();
-    mocha.addFile(join(__dirname,'./test.ts'))
+    mocha.addFile(join(__dirname,'./test'))
     mocha.run(function (failures) {
         process.exitCode = failures ? 1 : 0;
     });
