f63fbe5624762d1f6edc5a607a1f10ea3527361a
yanpred <yaniv@praedicta.com>
2019-06-30 19:40:32 +0300

    add timeout parameter

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
+import * as Mocha from "mocha";
+import { join } from "path";
 
-import * as Mocha from 'mocha'
-import {join} from 'path'
-
-function runTest(url: string, tests: { name: string, tests: { type: string, query: string, expect: any }[] }[]) {
-    global['specs'] = { url, tests };
-    var mocha = new Mocha();
-    mocha.addFile(join(__dirname,'./test'))
-    mocha.run(function (failures) {
-        process.exitCode = failures ? 1 : 0;
+function runTest(
+  url: string,
+  tests: {
+    name: string;
+    tests: { type: string; query: string; expect: any }[];
+  }[]
+) {
+  return new Promise((reslove, reject) => {
+    // tslint:disable-next-line: no-string-literal
+    global["specs"] = { url, tests };
+    const mocha: Mocha = new Mocha();
+    mocha.addFile(join(__dirname, "./test"));
+    mocha.run(failures => {
+      reslove(failures);
     });
+  });
 }
 
-
-export {runTest}
-
-
+export { runTest };
--- src/test.ts
+++ src/test.ts
@@ -1,33 +1,51 @@
-declare type Test = { name: string, tests: {desc:string, type: string, query: string, expect: any, propertyPath: string }[] }
-import { expect } from 'chai'
-const url: string = global['specs'].url
-const tests: Test[] = global['specs'].tests
-const request = require('supertest')(url);
+declare type Test = {
+  name: string;
+  tests: {
+    desc: string;
+    type: string;
+    query: string;
+    expect: any;
+    propertyPath: string;
+    timeout: number;
+  }[];
+};
+import { expect } from "chai";
+// tslint:disable-next-line: no-string-literal
+const url: string = global["specs"].url;
+// tslint:disable-next-line: no-string-literal
+const tests: Test[] = global["specs"].tests;
+const request = require("supertest")(url);
+console.log(tests);
+
 for (const test of tests) {
-    describe(test.name, () => {
-        test.tests.forEach(test => {
-            it(test.desc, (done) => {
-                request.post('/graphql')
-                    .send({
-                        query: test.query
-                    })
-                    .expect(200)
-                    .end((err, res) => {
-                        let result = res.body;
-                        if (test.propertyPath)
-                            test.propertyPath.split('.').forEach(prop => result = result[prop]);
+  describe(test.name, () => {
+    test.tests.forEach(test => {
+      console.log(test.timeout);
+      it(test.desc, done => {
+        request
+          .post("/graphql")
+          .send({
+            query: test.query
+          })
+          .expect(200)
+          .end((err, res) => {
+            let result = res.body;
+            if (test.propertyPath) {
+              test.propertyPath
+                .split(".")
+                .forEach(prop => (result = result[prop]));
+            }
 
-                        if (err) return done(err);
-                        let exp = expect(result);
-                        const ops = test.type.split('.')
-                        ops.slice(0, -1).forEach(t => exp = exp[t])
-                        exp[ops[ops.length - 1]](test.expect)
-                        done();
-                    })
-            })
-        });
-    })
+            if (err) {
+              return done(err);
+            }
+            let exp = expect(result);
+            const ops = test.type.split(".");
+            ops.slice(0, -1).forEach(t => (exp = exp[t]));
+            exp[ops[ops.length - 1]](test.expect);
+            done();
+          });
+      }).timeout(test.timeout || 2000);
+    });
+  });
 }
-
-
-
